2cb24ed2bdb5ad6adf31cfc5f6a3c1e6bbfb6cc5
Jake Geiser <geiser.jt@gmail.com>
2022-07-22 01:17:42 -0500

    cleaned up and add http

---
+++ src/app/services/api-http.service.ts
@@ -0,0 +1,21 @@
+// Angular Modules
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { validString } from 'src/utils/string-util';
+@Injectable()
+export class ApiHttpService {
+  constructor(
+    // Angular Modules
+    private http: HttpClient
+  ) { }
+  public get(url: string, options?: any) {
+    return this.http.get(url, options);
+  }
+
+  public search(searchInput: string){
+    if(!validString(searchInput)){return;}
+    let url = `https://us.openfoodfacts.org/cgi/search.pl?search_terms=${searchInput}&search_simple=1&action=process&json=true`;
+    return this.get(url);
+  }
+  
+}
---
+++ src/utils/string-util.ts
@@ -0,0 +1,4 @@
+export function validString(searchInput:string) {
+    // TODO add validation
+    return true;
+}
